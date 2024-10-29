4ac1b83350d8a9320674fcd299b97f0ce6ab8f25
Alejandro Molina Huatuco <carlos.molina@utec.edu.pe>
2024-10-28 20:07:05 -0500

    feat: Implementacion del flujo de inicio de sesion y registro de usuario con la validaciones

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,15 +1,82 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from '../../../shared/components/footer/footer.component';
 import { NavbarHomeComponent } from '../../../shared/components/navbar-home/navbar-home.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink, FooterComponent, NavbarHomeComponent],
+  imports: [
+    RouterLink,
+    FooterComponent,
+    NavbarHomeComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSnackBarModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  private fb = inject(FormBuilder);
+  private snackBar = inject(MatSnackBar);
+  private router = inject(Router);
 
+  constructor() {
+    this.registerForm = this.fb.group({
+      fullName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required]]
+    }, { validators: this.mustMatch('password', 'confirmPassword') });
+  }
+
+  mustMatch(password: string, confirmPassword: string) {
+    return (formGroup: FormGroup) => {
+      const passControl = formGroup.controls[password];
+      const confirmPassControl = formGroup.controls[confirmPassword];
+
+      if (confirmPassControl.errors && !confirmPassControl.errors['mustMatch']) {
+        return;
+      }
+
+      if (passControl.value !== confirmPassControl.value) {
+        confirmPassControl.setErrors({ mustMatch: true });
+      } else {
+        confirmPassControl.setErrors(null);
+      }
+    };
+  }
+
+
+  controlHasError(control: string, error: string) {
+    return this.registerForm.controls[control].hasError(error);
+  }
+
+  private showSnackBar(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 3000,
+      horizontalPosition: 'center',
+      verticalPosition: 'bottom',
+    });
+  }
+
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const userData = this.registerForm.value;
+      console.log("Datos de registro: ", userData);
+      this.showSnackBar('Registro exitoso');
+      this.router.navigate(['/auth/login']);
+    }
+  }
 }
